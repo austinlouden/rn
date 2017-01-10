0342178076a839654750fab61ef131fbbe7c374b
Austin Louden <alouden@pinterest.com>
2017-01-10 02:00:26 -0800

    add CategoryPage.js

---
+++ CategoryPage.js
@@ -0,0 +1,14 @@
+'use strict';
+
+import React, { Component } from 'react'
+import {
+  Text
+} from 'react-native';
+
+class CategoryPage extends Component {
+  render() {
+    return <Text>Hello World (Again)</Text>;
+  }
+}
+
+module.exports = CategoryPage;
