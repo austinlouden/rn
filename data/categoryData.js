9a5f4fa5ba3d150dacdbc01a05cc5e4e57238581
Austin Louden <alouden@pinterest.com>
2017-01-10 23:09:40 -0800

    add category data file

---
+++ data/categoryData.js
@@ -0,0 +1,10 @@
+export default categoryData = [
+  {
+    "category": "Gin",
+    "description": "asdaiji as a djiasd"
+  },
+  {
+    "category": "Whiskey",
+    "description": "asdaiji as a djiasd"
+  },
+];
